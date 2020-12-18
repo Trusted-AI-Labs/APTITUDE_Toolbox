ff033d769e86d9d494dd7d96f4afe505f031a655
Jonathan Samelson <jonathan@samelson.info>
2020-12-18 11:34:00 +0100

    Docstring & types

--- bboxes_2d_detector.py
+++ bboxes_2d_detector.py
@@ -1,13 +1,16 @@
+import numpy as np
 from abc import ABC, abstractmethod
+
 from pytb.detection.detector import Detector
+from pytb.output.bboxes_2d import BBoxes_2D
 
 class BBoxes2DDetector(Detector, ABC):
 
-    def __init__(self, detector_parameters):
-        """
-        Initiliazes the detectors with the given parameters.
-        :param detector_parameters: A dictionary containing the related detector's parameters
-        :return: boolean
+    def __init__(self, detector_parameters: dict):
+        """Initiliazes the detectors with the given parameters.
+
+        Args:
+            detector_parameters (dict): A dictionary containing the related detector's parameters
         """
         super().__init__()
         self.pref_implem = detector_parameters["BBoxes2DDetector"]["pref_implem"]
@@ -17,11 +20,13 @@ class BBoxes2DDetector(Detector, ABC):
         self.input_height = detector_parameters["BBoxes2DDetector"]["input_height"]
 
     @abstractmethod
-    def detect(self, org_frame):
-        """
-        Performs an inference on the given frame. 
-        Returns a set of 2DBox detections of the detected objects.
-        :param org_frame: The given frame to infer detections
-        :return: An object of the class 2DBoxes with the inference result
+    def detect(self, org_frame: np.ndarray) -> BBoxes_2D:
+        """Performs an inference on the given frame. 
+
+        Args:
+            org_frame (np.ndarray): The given frame to infer detections
+
+        Returns:
+            BBoxes_2D:A set of 2DBBoxes detections of the detected objects
         """
         pass
