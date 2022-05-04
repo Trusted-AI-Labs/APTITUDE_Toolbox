53c0619219449f868a1e22961f4b036bc2383c34
Jonathan Samelson <jonathan@samelson.info>
2022-05-04 17:19:30 +0200

    Updated validator for MaskRCNN and improved implem for inference with local model weights

--- pytb/detection/bboxes/bboxes_2d_detector/mask_rcnn/mrcnn.py
+++ pytb/detection/bboxes/bboxes_2d_detector/mask_rcnn/mrcnn.py
@@ -20,38 +20,23 @@ class MRCNN(BBoxes2DDetector):
             detector_parameters (dict): A dictionary containing the YOLO detector parameters
         """
         super().__init__(detector_parameters)
-        self.conf_thresh = detector_parameters["MRCNN"].get("conf_thresh", 0)
-        self.nms_thresh = detector_parameters["MRCNN"].get("nms_thresh", 0)
-        self.nms_across_classes = detector_parameters["MRCNN"].get("nms_across_classes", True)
+        self.use_coco = detector_parameters["MRCNN"].get("use_coco_weights", True)
         self.gpu = detector_parameters["MRCNN"].get("GPU", False)
-        self.half_precision = detector_parameters["MRCNN"].get("half_precision", False)
 
-        log.debug("GPU set to {} and half precision set to {}."
-                  .format(self.gpu, self.half_precision))
+        log.debug("GPU set to {}.".format(self.gpu))
 
         log.debug("Mask-RCNN {} implementation selected.".format(self.pref_implem))
 
-        if self.pref_implem == "torch-resnet50_pretrained":
-            self.net = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
+        if self.pref_implem == "torch-resnet50":
+            if self.use_coco:
+                self.net = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
+            else:
+                self.net = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=False)
+                self.net.load_state_dict(torch.load(self.model_path))
             if self.gpu:
                 self.net.cuda()
             else:
                 self.net.cpu()
-            self.net.conf = self.conf_thresh
-            self.net.iou = self.nms_thresh
-            self.net.agnostic = self.nms_across_classes
-            self.net.eval()
-
-        elif self.pref_implem == "torch-resnet50_from_file":
-            self.net = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=False)
-            self.net.load_state_dict(torch.load(self.model_path))
-            if self.gpu:
-                self.net.cuda()
-            else:
-                self.net.cpu()
-            self.net.conf = self.conf_thresh
-            self.net.iou = self.nms_thresh
-            self.net.agnostic = self.nms_across_classes
             self.net.eval()
 
         else:
@@ -66,13 +51,7 @@ class MRCNN(BBoxes2DDetector):
         Returns:
             BBoxes2D: A set of 2DBBoxes of the detected objects.
         """
-        if self.pref_implem == "torch-resnet50_pretrained":
-            frame = frame.astype('float32') / 255.0
-            frame = torch.from_numpy(frame).permute(2, 0, 1)
-            if self.gpu:
-                frame = frame.cuda()
-            output = self._detect_torch_resnet50_pretrained(frame)
-        elif self.pref_implem == "torch-resnet50_from_file":
+        if self.pref_implem == "torch-resnet50":
             frame = frame.astype('float32') / 255.0
             frame = torch.from_numpy(frame).permute(2, 0, 1)
             if self.gpu:
@@ -84,20 +63,6 @@ class MRCNN(BBoxes2DDetector):
 
         return output
 
-    def _setup_cv2(self):
-        if self.gpu:
-            self.net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
-            if self.half_precision:
-                self.net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA_FP16)
-                log.debug("OpenCV with DNN_BACKEND_CUDA target CUDAFP16.")
-            else:
-                self.net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
-                log.debug("OpenCV with DNN_BACKEND_CUDA target CUDA.")
-        else:
-            self.net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
-            self.net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
-            log.debug("OpenCV with DNN_BACKEND_OPENCV and target CPU.")
-
     def _detect_torch_resnet50_pretrained(self, org_frame) -> BBoxes2D:
         start = default_timer()
         with torch.no_grad():
@@ -108,11 +73,6 @@ class MRCNN(BBoxes2DDetector):
             #masks = predictions[0]['masks'].to('cpu').numpy()
         end = default_timer()
 
-        #results = np.array(output.xyxy[0].cpu())
-
-        # print(boxes)
-        # print(labels)
-        # print(scores)
         bboxes = BBoxes2D((end - start), boxes, labels.astype(int), scores,
                           self.input_width, self.input_height, "x1_y1_x2_y2")
         bboxes.to_xt_yt_w_h()
